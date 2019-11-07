5b91dc9a05167974a936b4331c64b8ec77cd6ebc
cweng1 <chengze-weng@uiowa.edu>
2019-11-07 17:14:41 -0600

    read,delete,add successfully!!!

--- app.py
+++ app.py
@@ -7,23 +7,21 @@ from flask_sqlalchemy import SQLAlchemy
 import pymysql
 import secrets
 
-
 conn = "mysql+pymysql://{0}:{1}@{2}/{3}".format(secrets.dbuser, secrets.dbpass, secrets.dbhost, secrets.dbname)
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='SuperSecretKey'
 app.config['SQLALCHEMY_DATABASE_URI'] = conn
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False # silence the deprecation warning
 db = SQLAlchemy(app)
 
 class cweng1_pokemonapp(db.Model):
-    pokemonId= db.Column(db.Integer, primary_key=True)
+    pokemonId = db.Column(db.Integer, primary_key=True)
     pokemon_name = db.Column(db.String(255))
-    maximum_cp = db.Column(db.String(20))
+    maximum_cp = db.Column(db.String(255))
 
     def __repr__(self):
-        return "ID: {0} | pokemon name: {1} | maximum cp: {2}".format(self.pokemonId, self.pokemon_name, self.maximum_cp)
+        return "id: {0} | pokemon name: {1} | maximum cp: {2}".format(self.pokemonId, self.pokemon_name, self.maximum_cp)
 
 class PokemonForm(FlaskForm):
     pokemon_name = StringField('Pokemon Name:', validators=[DataRequired()])
@@ -32,53 +30,53 @@ class PokemonForm(FlaskForm):
 
 @app.route('/')
 def index():
-    all_pokemon = cweng1_pokemonapp.query.all()
-    return render_template('index.html', pokemon=all_pokemon, pageTitle='Pokemon')
+    all_pokemons = cweng1_pokemonapp.query.all()
+    return render_template('index.html', pokemon=all_pokemons, pageTitle='New Pokemon')
 
-@app.route('/pokemon/new', methods=['GET','POST'])
+@app.route('/pokemon/new', methods=['GET', 'POST'])
 def add_pokemon():
     form = PokemonForm()
     if form.validate_on_submit():
         pokemon = cweng1_pokemonapp(pokemon_name=form.pokemon_name.data, maximum_cp=form.maximum_cp.data)
         db.session.add(pokemon)
         db.session.commit()
-        return redirect("/")
+        return redirect('/')
 
-    return render_template('add_pokemon.html', form=form, pageTitle='Add A New Pokemon', legend='Add A New Pokemon')
+    return render_template('add_pokemon.html', form=form, pageTitle='Add A New Pokemon', legend="Add A New Pokemon")
 
 @app.route('/pokemon/<int:pokemon_Id>', methods=['GET','POST'])
 def pokemon(pokemon_Id):
-    pokemon = cweng1_pokemonapp.query.get_or_404(pokemon_Id)
-    return render_template('pokemon.html', form=pokemon, pageTitle='pokemon Details')
+    pokemons = cweng1_pokemonapp.query.get_or_404(pokemon_Id)
+    return render_template('pokemon.html', form=pokemons, pageTitle='Pokemon Details')
 
 @app.route('/pokemon/<int:pokemon_Id>/update', methods=['GET','POST'])
 def update_pokemon(pokemon_Id):
     pokemon = cweng1_pokemonapp.query.get_or_404(pokemon_Id)
-    form = FriendForm()
+    form = PokemonForm()
     if form.validate_on_submit():
-        pokemon.pokemon_name = form.pokemon_name
-        pokemon.maximum_cp = form.maximum_cp
+        pokemon.pokemon_name = form.pokemon_name.data
+        pokemon.maximum_cp = form.maximum_cp.data
         db.session.commit()
-        flash('Pokemon has been updated.')
+        flash('The pokemon has been updated.')
         return redirect(url_for('pokemon', pokemon_Id=pokemon.pokemonId))
-    
-    form.pokemon_name.data = pokemon.pokemon_name
-    form.maximum_cp.data = pokemon.maximum_cp
-    return render_template('add_pokemon.html', form=form, pageTitle='Update Post',
-                            legend="Update A Pokemon")
+    elif request.method == 'GET':
+        form.pokemon_name.data = pokemon.pokemon_name
+        form.maximum_cp.data = pokemon.maximum_cp
 
 
-@app.route('/pokemon/<int:pokemon_Id>/delete', methods=['POST'])
+    return render_template('add_pokemon.html', form=form, pageTitle='Update Post', legend="Update A Pokemon")
+
+@app.route('/delete_pokemon/<int:pokemon_Id>', methods=['POST'])
 def delete_pokemon(pokemon_Id):
-    if request.method == 'POST':
+    if request.method == 'POST': #if it's a POST request, delete the friend from the database
         pokemon = cweng1_pokemonapp.query.get_or_404(pokemon_Id)
         db.session.delete(pokemon)
         db.session.commit()
         flash('Pokemon was successfully deleted!')
         return redirect("/")
-    else:
-        return redirect("/")
 
+    else: #if it's a GET request, send them to the home page
+        return redirect("/")
 
 if __name__ == '__main__':
     app.run(debug=True)
